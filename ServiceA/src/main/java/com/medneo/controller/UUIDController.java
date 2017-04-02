783e467ad3593d690de9666b422eab456ef22614
mohammad <shalabi67@hotmail.com>
2017-04-02 13:37:19 +0200

    finalize the project

--- ServiceA/src/main/java/com/medneo/controller/UUIDController.java
+++ ServiceA/src/main/java/com/medneo/controller/UUIDController.java
@@ -3,6 +3,7 @@ package com.medneo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,13 @@ public class UUIDController {
 	
 	@PostMapping("")
 	public ResponseEntity<Void> sendMetadata() {
+		uuidService.sendMetadata();
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+	
+	@GetMapping("")	
+	public ResponseEntity<Void> sendMetadataExample() {
+		//TODO: this is just to simulate the scenario, it is not needed.
 		uuidService.sendMetadata();
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
